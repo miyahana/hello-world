513d64cd31daac3d69b33e2e312fc36d073e0aea
g13923mh <g13923mh@s30418.tsuda.ac.jp>
2015-04-10 11:08:24 +0900

    Hello WorldとGood Afternoonを追加しました

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g13923mh
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("こんにちは世界");
+        System.out.println(10+5);
+        System.out.println("Hello World");
+        System.out.println("Good Afternoon");
+    }
+}
